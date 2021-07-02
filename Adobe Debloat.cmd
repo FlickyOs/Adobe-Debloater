c4b45004a6de4d116980f1881f861fab0c6beaba
Jason <jasonsc.peterson@gmail.com>
2021-07-02 01:09:19 -0500

    Multiple Changes, Overall Improvements
    + Improve readability
    - Quit calling color codes
    + Detect various inputs of yes and no: y, Y, yes, Yes, YES, n, N, no, No, and NO
    + Add an Admin Check
    
    ***Note: Unsure if folders are actually deleted, so partially untested.***

--- Adobe Debloat.cmd
+++ Adobe Debloat.cmd
@@ -1,6 +1,6 @@
 echo off
 color F0
-title Adobe Debloater
+title Adobe Debloater by Flicky
 mode con: cols=92 lines=21
 
 setlocal enabledelayedexpansion
@@ -8,102 +8,74 @@ for /f "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
   set "del=%%a"
 )
 
-cls
-call :colortext F0 "This script was made by" 
-call :colortext F5 " Flicky" && echo.
+:admincheck
+cls && echo Checking for Administrator Permissions. && timeout 1 >nul && cls && echo Checking for Administrator Permissions.. && timeout 1 >nul && cls && echo Checking for Administrator Permissions... && timeout 1 >nul && cls
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+	goto start
+    ) else (
+	cls
+        echo Error: Please Run As Administrator.
+	timeout 2 >nul
+	exit /b 1
+    )
+
+:start
+cls && echo This utility was made by [36m4Flicky
 timeout 3 > nul
 echo.
-echo This Script/Utility/Application to debloat Adobe Programs.
-echo.
-echo It removes Adobe pre-installed unnecessary Stuff like:CameraProfiles,LensProfiles,Cache and ETC.
+echo [30mIts purpose is to debloat Adobe programs.
+echo Debloating pre-installed stuff like: CameraProfiles, LensProfiles, Cache, etc.
 timeout 7 > nul
 
 :main
-cls
-echo Credits to:(canonez,Phlegm)
+cls && echo [30mThis utility allows you to choose what you want to remove.
+echo [31mHowever, keep in mind that it's not REVERSIBLE. You've been forewarned.
+echo [30mAre you sure you want to continue?
 echo.
-echo (Twitter.com/Flicky_VFX)
+echo Credits to: canonez, Phlegm, + revisitable
+echo [36m4https://twitter.com/Flicky_VFX[30m
 echo.
-call :colortext F0 "This script will allow you to choose what you want to remove." && echo .
-echo Keep in mind that its not REVERSIBLE,Are you sure you want to continue?
+set /p main= 
+if %main% == "y" "Y" "yes" "Yes" "YES" goto cameraprofiles
+if %main% == "n" "N" "no" "No" "NO" cls && echo Exiting. && timeout 1 >nul && cls && echo Exiting.. && timeout 1 >nul && cls && echo Exiting... && timeout 1 >nul && cls && exit
+
+:cameraprofiles
+cls && echo [36m4Delete [30mthe CameraProfiles folder? (y / n)
 echo.
-set /p con=
-if /i "%con%" == "no" cls && echo Exiting. && timeout 1 >nul && cls && echo Exiting.. && timeout 1 >nul && cls && echo Exiting... && timeout 1 >nul && cls && exit
-if /i "%con%" == "yes" (
-       goto debloat
-) else (
-     cls && echo Misspell detected.
-     timeout 2 >nul
-     cls && echo Returning. && timeout 1 >nul && cls && echo Returning.. && timeout 1 >nul && cls && echo Returning... && timeout 1 >nul && cls
-     goto :main
-)
-
-:debloat
-cls
-call :colortext F5 "Remove" && echo  CameraProfiles? (Useless)
 set /p con= 
-if /i "%con%" == "yes" goto :CameraProfiles
-if /i "%con%" == "no" ^ && cls
+if %con% == "y" "Y" "yes" "Yes" "YES" Del /s /f /q C:\ProgramData\Adobe\CameraRaw\CameraProfiles && @RD /S /Q "C:\ProgramData\Adobe\CameraRaw\CameraProfiles && goto lensprofiles
+if %con% == "n" "N" "no" "No" "NO" goto lensprofiles
 
-:CameraProfiles
-Del /s /f /q C:\ProgramData\Adobe\CameraRaw\CameraProfiles
-@RD /S /Q "C:\ProgramData\Adobe\CameraRaw\CameraProfiles
-
-cls && cls
-call :colortext F5 "Remove" && echo  LensProfiles? (Useless)
+:lensprofiles
+cls && echo [36m4Delete [30mthe LensProfiles folder? (y / n)
+echo.
 set /p con= 
-if /i "%con%" == "yes" goto :LensProfiles
-if /i "%con%" == "no" ^ && cls
+if %con% == "y" "Y" "yes" "Yes" "YES" Del /s /f /q C:\ProgramData\Adobe\CameraRaw\LensProfiles && @RD /S /Q "C:\ProgramData\Adobe\CameraRaw\LensProfiles && goto adobetutorials
+if %con% == "n" "N" "no" "No" "NO" goto adobetutorials
 
-:LensProfiles
-Del /s /f /q C:\ProgramData\Adobe\CameraRaw\LensProfiles
-@RD /S /Q "C:\ProgramData\Adobe\CameraRaw\LensProfiles
-
-cls && cls
-call :colortext F5 "Remove" && echo  All Adobe Tutorials?
+:adobetutorials
+cls && echo [36m4Remove [30mAll Adobe Tutorials? (y / n)
+echo.
 set /p con= 
-if /i "%con%" == "yes" goto :Adobe Tutorials
-if /i "%con%" == "no" ^ && cls
+if %con% == "y" "Y" "yes" "Yes" "YES" Del /s /f /q C:\Users\Public\Documents\Adobe && @RD /S /Q "C:\Users\Public\Documents\Adobe && goto mediacachefiles
+if %con% == "n" "N" "no" "No" "NO" goto mediacachefiles
 
-:Adobe Tutorials
-Del /s /f /q C:\Users\Public\Documents\Adobe
-@RD /S /Q "C:\Users\Public\Documents\Adobe
-
-cls && cls
-call :colortext F5 "Remove" && echo  Media Cache Files? (Adobe Cache)
-set /p con=
-if /i "%con%" == "yes" goto :Media Cache Files
-if /i "%con%" == "no" ^ && cls
-
-:Media Cache Files
-Del /s /f /q C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache Files
-@RD /S /Q "C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache Files
-Del /s /f /q C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache
-@RD /S /Q "C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache
-
-cls && cls
-call :colortext F5 "Remove" && echo  Adobe Temp Cache And Prefetch?
+:mediacachefiles
+cls && echo [36m4Remove [30mMedia Cache Files? (y / n)
+echo.
 set /p con= 
-if /i "%con%" == "yes" goto :delete
-if /i "%con%" == "no" ^ && cls
+if %con% == "y" "Y" "yes" "Yes" "YES" Del /s /f /q C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache Files && @RD /S /Q "C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache Files && Del /s /f /q C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache && @RD /S /Q "C:\Users\%username%\AppData\Roaming\Adobe\Common\Media Cache && goto tempcacheprefetch
+if %con% == "n" "N" "no" "No" "NO" goto tempcacheprefetch
 
-:delete
-del /s /f /q c:\windows\temp\*.*
-rd /s /q c:\windows\temp
-md c:\windows\temp
-del /s /f /q C:\WINDOWS\Prefetch
-del /s /f /q %temp%\*.*
-rd /s /q %temp%
-md %temp%
+:tempcacheprefetch
+cls && echo [36m4Remove [30mAdobe Temp Cache And Prefetch? (y / n)
+echo.
+set /p con= 
+if %con% == "y" "Y" "yes" "Yes" "YES" del /s /f /q c:\windows\temp\*.* && rd /s /q c:\windows\temp && md c:\windows\temp && del /s /f /q C:\WINDOWS\Prefetch && del /s /f /q %temp%\*.* && rd /s /q %temp% && md %temp% && goto exit
+if %con% == "n" "N" "no" "No" "NO" goto exit
 
 :exit
-cls && cls
-call :colortext F0 "Your Adobe Products is now " && call :colortext F6 " debloated" && echo. && timeout 2 > nul
-cls && echo Exiting. && timeout 1 >nul && cls && echo Exiting.. && timeout 1 >nul && cls && echo Exiting... && timeout 1 >nul && cls && exit
-
-
-:colortext
-echo off
-<nul set /p ".=%del%" > "%~2"
-findstr /v /a:%1 /R "^$" "%~2" nul
-del "%~2" > nul 2>&1
+cls && echo [33m4Debloating [30mcomplete.
+echo Thanks for using [36m4Flicky's [30mAdobe Debloating Utility! && timeout 4 > nul && cls
+echo [30mExiting. && timeout 1 >nul && cls && echo Exiting.. && timeout 1 >nul && cls && echo Exiting... && timeout 1 >nul && cls && exit
